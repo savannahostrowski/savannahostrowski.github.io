4a07d129786d7974fb042aaaafd8d29a689a454b
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-02 16:33:28 -0500

    created smileyface function

--- smileyface.js
+++ smileyface.js
@@ -3,6 +3,8 @@ var context = canvas.getContext('2d');
 var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
 
+
+
 function drawCircle(centerX, centerY, radius, fillColour, outlineColour, endAngle){
 	context.beginPath();
 	context.arc(centerX, centerY, radius, 0, endAngle, false);
@@ -13,8 +15,15 @@ function drawCircle(centerX, centerY, radius, fillColour, outlineColour, endAngl
 	context.stroke();
 }
 
-drawCircle(centerX, centerY, 120, 'yellow', 'black', 2 * Math.PI);
-drawCircle((centerX + 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
-drawCircle((centerX - 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
-drawCircle(centerX, (centerY + 40), 50, 'black', 'black', Math.PI);
+function drawSmiley(centerX, centerY){
+	
+	drawCircle(centerX, centerY, 120, 'yellow', 'black', 2 * Math.PI);
+	drawCircle((centerX + 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
+	drawCircle((centerX - 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
+	drawCircle(centerX, (centerY + 40), 50, 'black', 'black', Math.PI);
 
+}
+
+drawSmiley(100, 100);
+drawSmiley(2, 20);
+drawSmiley(30, 30);
