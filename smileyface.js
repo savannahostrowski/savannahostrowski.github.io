b0a3fb2a3110b1cc2e027f353b59a19c4b51e9b3
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-02 16:52:38 -0500

    generalized smiley radius

--- smileyface.js
+++ smileyface.js
@@ -1,11 +1,9 @@
 var canvas = document.getElementById('Canvas');
 var context = canvas.getContext('2d');
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
+//var centerX = canvas.width / 2;
+//var centerY = canvas.height / 2;
 
-
-
-function drawCircle(centerX, centerY, radius, fillColour, outlineColour, endAngle){
+function drawCircle(centerX, centerY, radius, fillColour, outlineColour, endAngle) {
 	context.beginPath();
 	context.arc(centerX, centerY, radius, 0, endAngle, false);
 	context.fillStyle = fillColour;
@@ -15,15 +13,14 @@ function drawCircle(centerX, centerY, radius, fillColour, outlineColour, endAngl
 	context.stroke();
 }
 
-function drawSmiley(centerX, centerY){
-	
-	drawCircle(centerX, centerY, 120, 'yellow', 'black', 2 * Math.PI);
-	drawCircle((centerX + 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
-	drawCircle((centerX - 40), (centerY - 40), 10, 'black', 'black', 2 * Math.PI);
-	drawCircle(centerX, (centerY + 40), 50, 'black', 'black', Math.PI);
-
+function drawSmiley(centerX, centerY, radius) {	
+	drawCircle(centerX, centerY, radius, 'yellow', 'black', 2 * Math.PI);
+	drawCircle((centerX + (radius / 4)), (centerY - (radius / 4)), radius / 10, 'black', 'black', 2 * Math.PI);
+	drawCircle((centerX - (radius / 4)), (centerY - (radius / 4)), radius / 10, 'black', 'black', 2 * Math.PI);
+	drawCircle(centerX, (centerY + (radius / 4)), radius / 2, 'black', 'black', Math.PI);
 }
 
-drawSmiley(100, 100);
-drawSmiley(2, 20);
-drawSmiley(30, 30);
+drawSmiley(100, 100, 120);
+drawSmiley(250, 250, 60);
+drawSmiley(50, 50, 30);
+drawSmiley(25, 25, 25);
