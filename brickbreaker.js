9260cc6abf99ac4ea55e3052f7ee19ab2fdc76b6
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-07 21:11:43 -0500

    added paddle

--- brickbreaker.js
+++ brickbreaker.js
@@ -5,9 +5,14 @@ var dx = 2;
 var dy = 4;
 var canvas = document.getElementById('Canvas');
 var context = canvas.getContext('2d');
+var WIDTH;
+var HEIGHT;
+var intervalId = 0;
 
 function init(){
 	context = canvas.getContext('2d');
+	WIDTH = canvas.width;
+	HEIGHT = canvas.height;
 	return setInterval(draw, 10);
 }
 
@@ -28,22 +33,37 @@ function drawRect(x, y, w, h){
 function clear(){
 	context.clearRect(0, 0, canvas.height, canvas.width);
 }
-drawCircle(25, 25, 5, 'green', 'black');
-drawRect();
 
 // End of Library Code
+var paddlex; 
+var paddleh;
+var paddlew;
+
+function init_paddle(){
+	paddlex = WIDTH / 2;
+	paddleh = 7;
+	paddlew = 60;
+}
 
 function draw(){
 	clear();
-	drawCircle(x, y, 10);
+	drawCircle(x, y, 5);
+	drawRect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
 
-	if (x + dx > canvas.width || x + dx < 0)
-		dx = -dx
-	if (y + dy > canvas.height || y + dy < 0)
-		dy = -dy
-
+	if (x + dx > WIDTH|| x + dx < 0)
+		dx = -dx;
+	if (y + dy < 0)
+		dy = -dy;
+	else if (y + dy > HEIGHT){
+		if (x > paddlex && x < paddlex + paddlew)
+			dy = -dy
+		else
+			//game over and halt animations
+			clearInterval(intervalId);
+	}
 x += dx;
 y += dy;
 }
 
 init();
+init_paddle();
