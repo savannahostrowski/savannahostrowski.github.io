254a90744e724f6b87f09ec1a211838dcf331bd5
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-07 21:35:22 -0500

    fixed infinite bounce and create stop animation feature

--- brickbreaker.js
+++ brickbreaker.js
@@ -4,17 +4,10 @@ var y = 150;
 var dx = 2;
 var dy = 4;
 var canvas = document.getElementById('Canvas');
-var context = canvas.getContext('2d');
+var context;
 var WIDTH;
 var HEIGHT;
 var intervalId = 0;
-
-function init(){
-	context = canvas.getContext('2d');
-	WIDTH = canvas.width;
-	HEIGHT = canvas.height;
-	return setInterval(draw, 10);
-}
 
 function drawCircle(x, y, r){
 	context.beginPath();
@@ -33,6 +26,12 @@ function drawRect(x, y, w, h){
 function clear(){
 	context.clearRect(0, 0, canvas.height, canvas.width);
 }
+function init(){
+	context = canvas.getContext('2d');
+	WIDTH = canvas.width;
+	HEIGHT = canvas.height;
+	intervalId= setInterval(draw, 10);
+}
 
 // End of Library Code
 var paddlex; 
@@ -40,29 +39,32 @@ var paddleh;
 var paddlew;
 
 function init_paddle(){
-	paddlex = WIDTH / 2;
+	paddlex = WIDTH / 2
 	paddleh = 7;
 	paddlew = 60;
 }
 
-function draw(){
-	clear();
-	drawCircle(x, y, 5);
-	drawRect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
 
-	if (x + dx > WIDTH|| x + dx < 0)
-		dx = -dx;
-	if (y + dy < 0)
-		dy = -dy;
-	else if (y + dy > HEIGHT){
-		if (x > paddlex && x < paddlex + paddlew)
-			dy = -dy
-		else
-			//game over and halt animations
-			clearInterval(intervalId);
-	}
-x += dx;
-y += dy;
+function draw() {
+  clear();
+  drawCircle(x, y, 5);
+  drawRect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
+ 
+  if (x + dx > WIDTH || x + dx < 0)
+    dx = -dx;
+
+  if (y + dy < 0)
+    dy = -dy;
+  else if (y + dy > HEIGHT) {
+    if (x > paddlex && x < paddlex + paddlew)
+      dy = -dy;
+    else
+      //game over, so stop the animation
+      clearInterval(intervalId);
+  }
+ 
+  x += dx;
+  y += dy;
 }
 
 init();
